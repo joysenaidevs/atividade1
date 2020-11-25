e15a2230c7d938f2bd6f240aafe0ccff96cb2fbc
Joyce <joycessilva17@hotmail.com>
2020-11-25 00:22:30 -0300

    atividade 1

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace atividade1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada
+	        // Ano atual
+	        // Ano de nascimento
+
+
+            // Processamento
+	        // idade = ano atual - ano de nascimento
+	        //     verificar se a idade é maior ou igual a 16
+
+            // Saida
+	        // Exibir mensagem
+	        // voce pode votar caso a idade for maior ou igual a 16 caso contrario nao poderá votar
+
+
+            //Entrada
+
+            Console.WriteLine("Exercicio 1");
+
+            Console.WriteLine("Digite o ano atual");
+            int anoAtual = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o ano do seu nascimento");
+            int anoNascimento = int.Parse(Console.ReadLine());
+
+            //Processamento
+
+            int idade = anoAtual - anoNascimento;
+            Console.WriteLine(idade);
+
+            //Saida
+            
+            if(idade>=16){
+                Console.WriteLine("Voce pode ir votar!");
+
+            }else{
+                Console.WriteLine("Voce não pode ir votar!");
+            }
+
+        }
+    }
+}
